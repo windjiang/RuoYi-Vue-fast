932777dca06741dda807196a1330327fba1e5ddb
peter <372840384@qq.com>
2023-12-09 14:43:08 +0800

    修改基础信息

--- src/main/java/com/ruoyi/framework/config/SwaggerConfig.java
+++ src/main/java/com/ruoyi/framework/config/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package com.ruoyi.framework.config;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.swagger.annotations.ApiOperation;
 import io.swagger.models.auth.In;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -112,7 +114,7 @@ public class SwaggerConfig
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
-                .title("标题：若依管理系统_接口文档")
+                .title("标题：管理系统_接口文档")
                 // 描述
                 .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
                 // 作者信息
